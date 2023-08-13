18089dee078709f382393c724386b96c7e195a75
Gihan <gihanwijesinghe12@gmail.com>
2023-08-13 22:01:23 +0530

    creating cafes

--- src/components/cafe/cafes.tsx
+++ src/components/cafe/cafes.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import CafeService, { CafeResponse } from "../../services/cafeService";
+import CafeService, { CafePost, CafeResponse } from "../../services/cafeService";
+import { GridCellParams, GridColDef, GridRowModel } from "@mui/x-data-grid";
+import DataTable from "../common.tsx/dataTable";
+import { Button } from "@mui/material";
 
 const Cafes: React.FC = () => {
   const [loading, setLoading] = React.useState<boolean>(true);
@@ -10,8 +13,41 @@ const Cafes: React.FC = () => {
       .then((res) => setCafes(res))
       .finally(() => setLoading(false));
   }, []);
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "Name", width: 180, editable: true },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "employeesCount",
+      headerName: "Employees",
+      width: 180,
+      editable: false,
+      renderCell: (params: GridCellParams) => <Button>{params.value as any}</Button>,
+    },
+    { field: "location", headerName: "Location", width: 180, editable: true },
+  ];
 
-  return <div className="container">{loading ? <>Loading...</> : cafes.map((cafe) => <>{cafe.name}</>)}</div>;
+  const onRowUpdate = async (row: GridRowModel) => {
+    const post: CafePost = {
+      name: row.name,
+      description: row.description,
+      location: row.location,
+    };
+    if (row.isNew) {
+      await CafeService.postCafe(post);
+      return { ...row, employeesCount: 0 };
+    }
+  };
+
+  return (
+    <div className="container">
+      {loading ? <>Loading...</> : <DataTable rows={cafes} colums={columns} onRowUpdate={onRowUpdate} />}
+    </div>
+  );
 };
 
 export default Cafes;
--- src/services/cafeService.ts
+++ src/services/cafeService.ts
@@ -6,11 +6,29 @@ export interface CafeResponse {
   employeeCount: number;
 }
 
+export interface CafePost {
+  name: string;
+  description: string;
+  location: string;
+}
+
 const CafeService = {
   getCafes: () => {
     return fetch("https://localhost:7122/api/cafes")
       .then((res) => res.json())
       .then((res) => res as CafeResponse[]);
+  },
+
+  postCafe: (cafePost: CafePost) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(cafePost),
+    };
+
+    return fetch("https://localhost:7122/api/cafes", requestOptions)
+      .then((res) => res.json())
+      .then((res) => res as any);
   },
 };
 
